71a270cc6108d55c1677da3fb36eac9cd2558376
henriquegalotte <38058819+henriquegalotte@users.noreply.github.com>
2024-07-22 10:55:39 -0300

    Adicionando interações
    Adicionando interações com o JavaScript para que o escudo tenha o movimento de pulo/ adaptando o título do main (boas vindas)

--- js/app.js
+++ js/app.js
@@ -1,24 +1,43 @@
-const logoanimatespin = [
 
+
+// Config Param Rotação
+const logoanimatespin = [
     { transform: "rotate(0)" },
     { transform: "rotate(360deg)" },
-
 ];
 
-
+// Config Param Tempo e Qtd de vezes que vai repetir
 const logoanimatetime = {
     duration: 200,
     iterations: 2,
   };
-const logoanimate = document.querySelector(".logoanimate");
+// Config Param Pulo
+const animatejump = [
+        { transform: "translateY(0px)" },
+        {transform: "translateY(-100px)", offset:0.5,},
+        {transform: "translateY(0px)", offset:0.7,},
+        {transform: "translateY(0px)", offset:1.0,}
+];
+// Config Param Tempo e Qtd de vezes que vai repetir
+const jumptime = {
+    duration:1000,
+    iterations:1,
+};
 
+//Variável recebendo a classe para simplificar o uso sem digitar a linha toda do comando
+const logoanimate = document.querySelector(".logoanimate");
+//Variável recebendo a classe para simplificar o uso sem digitar a linha toda do comando
 const shieldanimate = document.querySelector(".shieldanimate");
 
-shieldanimate.addEventListener("click", () => {
-    shieldanimate.animate(logoanimatespin,logoanimatetime);
-});
 
+
+//Usando a variável acima/adicionando um evento para capturar a ação "click"
+//VARIÁVEL.animate(paramAção,paramTempoErepetição)
+shieldanimate.addEventListener("click", () => {
+    shieldanimate.animate(animatejump,jumptime);
+});
+//Usando a variável acima/adicionando um evento para capturar a ação "click"
+//VARIÁVEL.animate(paramAção,paramTempoErepetição)
 logoanimate.addEventListener("click",  () => {
-    logoanimate.animate(logoanimatespin,logoanimatetime);
-    
+    logoanimate.animate(logoanimatespin,logoanimatetime);   
 });
